72c8010a1f0fcb3bfc9823da2c841d20f024041b
Adam Murdoch <adam@gradle.com>
2018-02-26 10:40:50 +1100

    Reworked MSBuild discovery.

--- subprojects/ide-native/src/integTest/groovy/org/gradle/ide/visualstudio/fixtures/MSBuildVersionLocator.java
+++ subprojects/ide-native/src/integTest/groovy/org/gradle/ide/visualstudio/fixtures/MSBuildVersionLocator.java
@@ -19,6 +19,7 @@ package org.gradle.ide.visualstudio.fixtures;
 import org.gradle.nativeplatform.fixtures.AvailableToolChains;
 import org.gradle.nativeplatform.toolchain.internal.msvcpp.version.VswhereVersionLocator;
 import org.gradle.test.fixtures.file.TestFile;
+import org.gradle.util.VersionNumber;
 
 import java.io.File;
 
@@ -30,28 +31,12 @@ public class MSBuildVersionLocator {
     }
 
     public File getMSBuildInstall(AvailableToolChains.InstalledToolChain toolChain) {
+        VersionNumber vsVersion;
         if (toolChain instanceof AvailableToolChains.InstalledVisualCpp) {
             AvailableToolChains.InstalledVisualCpp visualCpp = (AvailableToolChains.InstalledVisualCpp) toolChain;
-            File msBuildDir = new File("C:/Program Files (x86)/MSBuild");
-            File msBuildExe = new File(msBuildDir, visualCpp.getVersion().getMajor() + ".0/Bin/amd64/MSBuild.exe");
-            if (!msBuildExe.isFile()) {
-                for (File f = msBuildExe; f != null; f = f.getParentFile()) {
-                    if (f.exists()) {
-                        System.out.println("-> FOUND " + f);
-                        System.out.println("CHILDREN:");
-                        String[] children = f.list();
-                        if (children != null) {
-                            for (String child : children) {
-                                System.out.println("  - " + child);
-                            }
-                        }
-                        break;
-                    }
-                }
-
-                throw new IllegalStateException(String.format("Could not find MSBuild for Visual Studio version %s. Expected in %s", visualCpp.getVersion(), msBuildExe));
-            }
-            return msBuildExe;
+            vsVersion = visualCpp.getVersion();
+        } else {
+            vsVersion = VersionNumber.version(15);
         }
 
         File vswhere = vswhereLocator.getVswhereInstall();
@@ -59,12 +44,12 @@ public class MSBuildVersionLocator {
             throw new IllegalStateException("vswhere tool is required to be installed");
         }
 
-        TestFile installDir = new TestFile(new TestFile(vswhere).exec("-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild", "-property", "installationPath").getOut().trim());
+        TestFile installDir = new TestFile(new TestFile(vswhere).exec("-version", String.format("[%s.0,%s.0)", vsVersion.getMajor(), vsVersion.getMajor() + 1), "-products", "*", "-requires", "Microsoft.Component.MSBuild", "-property", "installationPath").getOut().trim());
 
         // TODO: Remove the hardcoded version, see https://github.com/Microsoft/vswhere/issues/74
-        TestFile msbuild = installDir.file("MSBuild/15.0/Bin/MSBuild.exe");
+        TestFile msbuild = installDir.file("MSBuild/" + vsVersion.getMajor() + ".0/Bin/MSBuild.exe");
         if (!msbuild.exists()) {
-            throw new IllegalStateException("This test requires msbuild to be installed");
+            throw new IllegalStateException(String.format("This test requires MSBuild %s to be installed. Expected it to be installed at %s.", vsVersion.getMajor(), msbuild));
         }
         return msbuild;
     }
